1b6b52782c6b8b18eddb9b99f00e47977318efd8
bharathsurya021 <bharathsurya021@gmail.com>
2023-08-17 22:28:00 +0530

    header setup

--- portfolio/app/layout.js
+++ portfolio/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Header from '@/components/Header'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
       <body className={`bg-gray-50 relative text-gray-950 ${inter.className}`}>
         <div className='sm:w-[68.75rem] -z-10 bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem]'></div>
         <div className='bg-[#dbd7fb] -z-10 absolute md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem]'></div>
-      
+        <Header/>
         {children}
         </body>
     </html>
---
+++ portfolio/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="z-[999] relative">
+      <div className="fixed h-[4.5rem] w-full border-white border-opacity-40 bg-white bg-opacity-30 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:h-[3.5rem]"></div>
+    </header>
+  );
+};
+
+export default Header;
